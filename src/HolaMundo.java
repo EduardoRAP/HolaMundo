3e919fdd8c449b768f2e1042d26acd0681791eca
Eduardo Rivero de Aguilar <eduardo.rivero@live.u-tad.com>
2017-01-12 21:21:02 +0100

    NuevoMensaje

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println(esto es un bug2);
+		Sytem.out.println("Adios mundo");
 
 	}
 
