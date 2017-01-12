a10629d5c9d56748c70a8f09f448732080b737d4
Eduardo Rivero de Aguilar <eduardo.rivero@live.u-tad.com>
2017-01-12 22:03:57 +0100

    Update HolaMundo.java
    fetch

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println(esto es un bug2);
-		Sytem.out.println("Adios mundo");
+		Sytem.out.println("Modificacion fetch");
 
 	}
 
